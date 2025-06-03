f5f3e456234e66c3b3f26dd7f9ed50c98b6ecb0f
Nikolaus Draeger <nikolaus.draeger@gmail.com>
2025-06-02 19:29:22 -0700

    Gaussian initialization

--- scene/gaussian_model.py
+++ scene/gaussian_model.py
@@ -429,7 +429,8 @@ class GaussianModel:
         self._scaling = optimizable_tensors["scaling"]
         self._rotation = optimizable_tensors["rotation"]
 
-        self.tmp_radii = torch.cat((self.tmp_radii, new_tmp_radii))
+        if self.tmp_radii is not None:
+            self.tmp_radii = torch.cat((self.tmp_radii, new_tmp_radii))
         print(self.tmp_radii.shape)
         self.xyz_gradient_accum = torch.zeros((self.get_xyz.shape[0], 1), device="cuda")
         self.denom = torch.zeros((self.get_xyz.shape[0], 1), device="cuda")
